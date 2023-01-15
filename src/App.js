57921c0c2fb7463fed5aba3353a6ce41a887a9e9
LuisCruzVeldever <luiscruzv@outlook.com>
2023-01-15 18:27:35 -0500

    imprimir busqueda

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import React, { Component } from 'react';
 import Buscador from './componentes/Buscador';
 
 class App extends Component {
+
+  state = {
+    termino : 'Café'
+  }
+
+
+  datosBusqueda = (termino) => {
+    console.log(termino);
+  }
   render() { 
   return (
     <div className="app container">
       <div className="jumbotron">
         <p className="lead text-center">Buscador de Imágenes</p>
         <Buscador 
-        mensaje="Buscador.."
+        datosBusqueda={this.datosBusqueda}
         />
       </div>
+      {this.state.termino}
     </div>
   );
 }
--- src/componentes/Buscador.js
+++ src/componentes/Buscador.js
@@ -7,15 +7,17 @@ class Buscador extends Component {
     obtenerDatos = (e) => {
         e.preventDefault();
 
-        console.log(this.busquedaRef.current.value);
+        //tomamos el valor del input
+        const termino = this.busquedaRef.current.value;
 
+        //lo enviamos al componente principal
+        this.props.datosBusqueda(termino);
     }
 
     render() { 
         return ( 
              <form onSubmit={this.obtenerDatos}>
                 <div className="row">
-                    {this.props.mensaje}
                      <div className="form-group col-md-8">
                         <input ref={this.busquedaRef} type="text" className="form-control 
                         form-control-lg" placeholder="Busca tu Imagen.
