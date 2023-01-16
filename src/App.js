54e13ae538cf4113a29750980de01fe8ca2fcd3e
LuisCruzVeldever <luiscruzv@outlook.com>
2023-01-15 19:30:39 -0500

    ejecutar imagenes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
 import Buscador from './componentes/Buscador';
+import Resultado from './componentes/Resultado';
 
 class App extends Component {
 
   state = {
-    termino : 'Café'
+    termino : '',
+    imagenes : []
+  }
+
+  consultarApi = () => {
+    const termino = this.state.termino;
+
+    const url = `https://pixabay.com/api/?key=1732750-d45b5378879d1e877cd1d35a6&q=${termino}&per_page=30`;
+
+   // console.log(url);
+   fetch(url)
+   .then(respuesta => respuesta.json())
+   .then(resultado => this.setState({ imagenes : resultado.hits }) )
+
   }
 
   datosBusqueda = (termino) => {
     this.setState({
       termino
+  }, () => {
+    this.consultarApi();
   })
 }
   render() { 
@@ -21,7 +37,10 @@ class App extends Component {
         datosBusqueda={this.datosBusqueda}
         />
       </div>
-      {this.state.termino}
+     <Resultado 
+     imagenes={this.state.imagenes}
+     
+     />
     </div>
   );
 }
